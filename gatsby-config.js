d7c1e262b3dea6ac6aeaa9337ad28e99993cee03
Fidel Torres <fidel.torres@hazzas.com>
2020-11-07 20:01:30 +1100

    added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,24 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-prefetch-google-fonts',
+      options: {
+        fonts: [
+          {
+            family: 'Roboto Slab',
+            subsets: ['latin'],
+          },
+          {
+            family: 'Roboto',
+            variants: ['100', '400', '700'],
+            subsets: ['latin']
+          },
+        ],
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    // 'gatsby-plugin-offline',
   ],
 };
